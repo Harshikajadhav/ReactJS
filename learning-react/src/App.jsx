7446bac14085d60facd100af8a17f2e87eaf41f9
Harshikajadhav <harshika.jadhav@gmail.com>
2025-04-17 15:47:02 +0530

    update App.jsx
    Counter

--- learning-react/src/App.jsx
+++ learning-react/src/App.jsx
@@ -1,10 +1,18 @@
 import React, { useState } from 'react'
 
 const App = () => {
-  const [username, setUsername] = useState('')
+  const [num, setNum] = useState(0)
+  const increase = () => {
+    setNum(num + 1)
+  }
+  const decrease = () => {
+    setNum(num - 1)
+  }
   return (
     <div>
-      <h1>Username is {}</h1>
+      <h3>Number is: {num}</h3>
+      <button onClick={increase}>Increament</button>
+      <button onClick={decrease}>Decreament</button>
     </div>
   )
 }
